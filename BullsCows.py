ebf726c03b7feb29608398937ca095f053f1819c
mertljan <jm.ostrava@gmail.com>
2021-07-31 16:11:36 +0200

    send

--- BullsCows.py
+++ BullsCows.py
@@ -1,5 +1,5 @@
-import random
-import help_game
+import sys
+import help_game as gm
 
 print("""
 Hi there!
@@ -13,10 +13,26 @@ Enter a number:
 
 def main():
 
-    num = generCisla()
-    sec_lst = getCisla(num)
-    print(sec_lst)
+    num = gm.generCisla()
+    sec_lst = gm.getCisla(num)
+    #print(sec_lst)
 
+    hrac_cislo = input("Zadej 4 místné číslo: ")
+    lst_hrac = list(hrac_cislo)
+
+
+    if len(lst_hrac) != 4:
+        print("Zadal jsem nevhodnou délu čísla")
+        sys.exit(-1)
+    elif not hrac_cislo.isdigit():
+        print("Číslo obsahuje nepovolené znaky")
+        sys.exit(-2)
+    elif int(lst_hrac[0]) == 0:
+        print("Číslo nesmí začínat nulou")
+        sys.exit(-3)
+
+    hrac = gm.game(sec_lst,lst_hrac)
+    print("cows: ",hrac[0]," bulls: ",hrac[1])
 
 
 
--- help_game.py
+++ help_game.py
@@ -19,3 +19,14 @@ def zrus_duplicity(num):
 def getCisla(num):
     return [int(i) for i in str(num)]
 
+def game(lst_sec:list,lst_gm:list):
+    hrac_vysl = [0, 0]
+    for pc, hrac in zip(lst_sec, lst_gm):
+        if int(hrac) in lst_sec:
+            hrac_vysl[0] += 1
+            if int(pc) == int(hrac):
+                hrac_vysl[1] += 1
+
+
+    return hrac_vysl
+

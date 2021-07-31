3534e05afb0f47c2daa1c24fb042fc7cf46ceda1
mertljan <jm.ostrava@gmail.com>
2021-07-31 13:51:54 +0200

    send

---
+++ BullsCows.py
@@ -0,0 +1,11 @@
+import random
+
+print("""
+Hi there!
+-----------------------------------------------
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+-----------------------------------------------
+Enter a number:
+-----------------------------------------------
+""")
